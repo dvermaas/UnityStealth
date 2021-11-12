aaac3cf28144c99d5a404b7c7436dd0e49200e57
dvermaas <31657287+dvermaas@users.noreply.github.com>
2021-11-12 11:53:06 +0100

    Update Tesla.cs
    Made it scale for any length array

--- PrisonEscape/Assets/Scripts/Tesla.cs
+++ PrisonEscape/Assets/Scripts/Tesla.cs
@@ -7,12 +7,11 @@ public class Tesla : MonoBehaviour
     public GameObject[] roadPieces;
     //private float spacing = 191.9f;
     private float spacing = 191f;
-    private float other_y;
+    private float smallest_z;
 
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
-        other_y = roadPieces[roadPieces.Length - 1].transform.position.z;
     }
 
     void Update()
@@ -24,12 +23,21 @@ public class Tesla : MonoBehaviour
         float dt = Time.deltaTime;
         foreach (GameObject road in roadPieces)
         {
+            // Scroll speed
             road.transform.Translate(0, 0, 20 * dt);
             if (road.transform.position.z >= 0)
             {
-                road.transform.position = new Vector3(0, 0, other_y - spacing);
+                // Paste road after the last roadpiece (scaleable for any length roadPieces)
+                smallest_z = 0;
+                foreach (GameObject road2 in roadPieces)
+                {
+                    if (road2.transform.position.z < smallest_z)
+                    {
+                        smallest_z = road2.transform.position.z;
+                    }
+                }
+                road.transform.position = new Vector3(0, 0, smallest_z - spacing);
             }
-            other_y = road.transform.position.z;
         }
     }
 }
